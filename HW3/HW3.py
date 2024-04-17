bb1990bc9dd016d97343cd9b33da725dccbc87a4
adicere <rutkovskaya.ea@gmail.com>
2024-04-17 15:25:15 +0300

    Car class with speed iterators

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,128 +1,111 @@
+!pip install openmeteo_requests
+import openmeteo_requests
+import datetime
+
 class IncreaseSpeed():
-  '''
-  Iterator for increasing the speed with the default step of 10 km/h
-  You can implement this one after Iterators FP topic
-
-  Constructor params: 
-    current_speed: a value to start with, km/h
-    max_speed: a maximum possible value, km/h
-
-  Make sure your iterator is not exceeding the maximum allowed value
-  '''
-
   def __init__(self, current_speed: int, max_speed: int):
     self.current = current_speed
     self.max = max_speed
   def __iter__(self):
-    return self 
+    return self
   def __next__(self):
-    self.current += 1
-    if self.current < self.max:
+    self.current += 10
+    if self.current <= self.max:
       return self.current
-    raise StopIteration
 
 class DecreaseSpeed():
-  '''
-  Iterator for decreasing the speed with the default step of 10 km/h
-  You can implement this one after Iterators FP topic 
-
-  Constructor params: 
-    current_speed: a value to start with, km/h
-
-  Make sure your iterator is not going below zero
-  '''
-
-  def __init__(self, current_speed: int):
-    pass
+  def __init__(self, current_speed: int, min_speed: int):
+    self.current = current_speed
+    self.min = min_speed
   def __iter__(self):
-    pass
+    return self
   def __next__(self):
-    pass
+    self.current -= 10
+    if self.current >= self.min:
+      return self.current
 
 class Car():
-  '''
-  Car class. 
-  Has a class variable for counting total amount of cars on the road (increased by 1 upon instance initialization).
 
-  Constructor params:
-    max_speed: a maximum possible speed, km/h
-    current_speed: current speed, km/h (0 by default)
-    state: reflects if the Car is in the parking or on the road
-
-  Methods:
-    accelerate: increases the speed using IncreaseSpeed() iterator either once or gradually to the upper_border
-    brake: decreases the speed using DecreaseSpeed() iterator either once or gradually to the lower_border
-    parking: if the Car is not already in the parking, removes the Car from the road
-    total_cars: show the total amount of cars on the road
-    show_weather: shows the current weather conditions
-  '''
+  cars = 0
 
   def __init__(self, max_speed: int, current_speed=0):
-    pass
-  
+    self.current = current_speed
+    self.max = max_speed
+    if current_speed<0:
+      print("Wrong speed value - number should be greater than 0")
+    if current_speed>0:
+      self.state = "On the road"
+      Car.cars +=1
+    else:
+      self.state = "Off the road"
 
   def accelerate(self, upper_border=None):
-    # check for state 
-    # create an instance of IncreaseSpeed iterator
-    # check if smth passed to upper_border and if it is valid speed value
-    # if True, increase the speed gradually iterating over your increaser until upper_border is met
-    # print a message at each speed increase
-    # else increase the speed once 
-    # return the message with current speed
-    pass
-
+    if self.state == "Off the road":
+      Car.cars+=1
+      self.state = "On the road"
+    if self.current == self.max:
+      print('Maximum speed is reached')
+      return
+    else:
+      if upper_border != None:
+        acceleration = IncreaseSpeed(self.current, upper_border)
+        while self.current < upper_border:
+          self.current = next(acceleration)
+          print(f"Driving at {self.current} km/h")
+      else:
+        acceleration = IncreaseSpeed(self.current, self.max)
+        self.current = next(acceleration)
+        return f'Speed increased up to {self.current} km/h'
 
   def brake(self, lower_border=None):
-    # create an instance of DecreaseSpeed iterator
-    # check if smth passed to lower_border and if it is valid speed value
-    # if True, decrease the speed gradually iterating over your decreaser until lower_border is met
-    # print a message at each speed decrease
-    # else increase the speed once 
-    # return the message with current speed
-    pass
+    if self.current == 0:
+      print('Speed is 0 km/h')
+      return
+    else:
+      if lower_border !=None:
+        braking=DecreaseSpeed(self.current, lower_border)
+        while self.current > lower_border:
+          self.current = next(braking)
+          print(f"Driving at {self.current} km/h")
+      else:
+        braking = DecreaseSpeed(self.current,0)
+        self.current = next(braking)
+        return f'Speed decreased down to {self.current} km/h'
 
-  # the next three functions you have to define yourself
-  # one of the is class method, one - static and one - regular method (not necessarily in this order, it's for you to think)
+  def parking(self):
+    if self.state == "Off the road":
+      return
+    Car.cars -= 1
+    self.current=0
+    self.state = "Off the road"
+    print(f'Car is parked')
 
-  def parking():
-    # gets car off the road (use state and class variable)
-    # check: should not be able to move the car off the road if it's not there
-    pass
+  @classmethod
+  def total_cars(cls):
+    print(f'There are {Car.cars} on the road')
 
-  def total_cars():
-    # displays total amount of cars on the road
-    pass
-
+  @staticmethod
   def show_weather():
-    # displays weather conditions
-    pass
+    openmeteo = openmeteo_requests.Client()
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+    "latitude": 33.8688, #Sydney 
+    "longitude": 151.2093,
+    "current": ["temperature_2m", "apparent_temperature", "rain", "wind_speed_10m"],
+    "wind_speed_unit": "ms",
+    "timezone": "Australia/Sydney"
+    }
 
-    # To use this API you have to install openmeteo_requests library'
-import openmeteo_requests
-import datetime
+    response = openmeteo.weather_api(url, params=params)[0]
 
+    current = response.Current()
+    current_temperature_2m = current.Variables(0).Value()
+    current_apparent_temperature = current.Variables(1).Value()
+    current_rain = current.Variables(2).Value()
+    current_wind_speed_10m = current.Variables(3).Value()
 
-openmeteo = openmeteo_requests.Client()
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-"latitude": 59.9386, # for St.Petersburg
-"longitude": 30.3141, # for St.Petersburg
-"current": ["temperature_2m", "apparent_temperature", "rain", "wind_speed_10m"],
-"wind_speed_unit": "ms",
-"timezone": "Europe/Moscow"
-}
-
-response = openmeteo.weather_api(url, params=params)[0]
-
-# The order of variables needs to be the same as requested in params->current!
-current = response.Current()
-current_temperature_2m = current.Variables(0).Value()
-current_apparent_temperature = current.Variables(1).Value()
-current_rain = current.Variables(2).Value()
-current_wind_speed_10m = current.Variables(3).Value()
-
-print(f"Current time: {datetime.fromtimestamp(current.Time()+response.UtcOffsetSeconds())} {response.TimezoneAbbreviation().decode()}")
-print(f"Current temperature: {round(current_temperature_2m, 0)} C")
-print(f"Current apparent_temperature: {round(current_apparent_temperature, 0)} C")
-print(f"Current rain: {current_rain} mm")
-print(f"Current wind_speed: {round(current_wind_speed_10m, 1)} m/s")
+    print(f"Current time: {datetime.datetime.fromtimestamp(current.Time()+response.UtcOffsetSeconds())} {response.TimezoneAbbreviation().decode()}")
+    print(f"Current temperature: {round(current_temperature_2m, 0)} C")
+    print(f"Current apparent_temperature: {round(current_apparent_temperature, 0)} C")
+    print(f"Current rain: {current_rain} mm")
+    print(f"Current wind_speed: {round(current_wind_speed_10m, 1)} m/s")
